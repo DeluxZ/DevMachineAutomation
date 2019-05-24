0a762f41e5bf260085cff9c2607bbe4f199ece35
Harrie Essing <harrie.essing@gmail.com>
2019-05-24 21:18:22 +0200

    Create work_dev_machine.ps1

---
+++ work_dev_machine.ps1
@@ -0,0 +1,50 @@
+# Description: Install develop machine for work
+# Author: Harrie Essing
+# Developer settings and apps for work
+
+Disable-UAC
+
+#--- Uninstall unecessary applications that come with Windows out of the box ---
+Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
+
+function removeApp {
+	Param ([string]$appName)
+	Write-Output "Trying to remove $appName"
+	Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
+	Get-AppXProvisionedPackage -Online | Where DisplayName -like $appName | Remove-AppxProvisionedPackage -Online
+}
+
+$Applications = @(
+	"Microsoft.BingFinance"
+	"Microsoft.3DBuilder"
+	"Microsoft.BingNews"
+	"Microsoft.BingSports"
+	"Microsoft.BingWeather"
+	"Microsoft.CommsPhone"
+	"Microsoft.Getstarted"
+	"Microsoft.WindowsMaps"
+	"*MarchofEmpires*"
+	"Microsoft.GetHelp"
+	"Microsoft.Messaging"
+	"*Minecraft*"
+	"Microsoft.MicrosoftOfficeHub"
+	"Microsoft.OneConnect"
+	"Microsoft.WindowsPhone"
+	"Microsoft.WindowsSoundRecorder"
+	"*Solitaire*"
+	"Microsoft.MicrosoftStickyNotes"
+	"Microsoft.Office.Sway"
+	"Microsoft.NetworkSpeedTest"
+	"Microsoft.FreshPaint"
+	"Microsoft.Print3D"
+	"*Autodesk*"
+	"*BubbleWitch*"
+    	"king.com*"
+	"*Plex*"
+);
+
+foreach ($app in $Applications) {
+    removeApp $app
+}
+
+Enable-UAC
